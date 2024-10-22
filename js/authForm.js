e6b65f3c9a994e4f0b534f8446506f7e39c299a1
Abdirahman <Abdirahmannadaara42@gmail.com>
2024-10-22 21:46:04 +0300

    I completed the sign In and Sgin up with localStorage

--- js/authForm.js
+++ js/authForm.js
@@ -13,8 +13,59 @@ const switchForm = document.querySelector('#switchForm')
 // * click 
 let signIn = true
 document.body.addEventListener('click', (e) => {
-    if (e.target.id != 'switchForm') return
-   
+    if (e.target.id != 'switchForm') return;
+    switchFormAuto()
+  
+})
+
+///?? form submit
+authForm.addEventListener('submit', function (e) {
+
+    e.preventDefault()
+    const user = {
+        username: signIn ? undefined : username.value,
+        email: email.value,
+        password: password.value,
+        confirmPassword: signIn ? undefined : confirmPassword.value
+
+    }
+    if (signIn) {
+
+        const users = JSON.parse(localStorage.getItem('users')) || []
+
+        const existingUser = users.find(user => user.password == password.value && user.email == email.value)
+        if(existingUser){
+
+            localStorage.setItem('onlineUser', JSON.stringify(existingUser))
+            alert('you are in')
+        }else {
+            alert('invalid credential')
+        }
+
+    } else {
+        const users = JSON.parse(localStorage.getItem('users')) || []
+
+        const existingUser = users.find(user => user.username == username.value && user.email == email.value)
+        if (existingUser) {
+            alert(`User ${username.value} is already exit`)
+            return;
+        }
+
+        users.push(user)
+        localStorage.setItem('users', JSON.stringify(users));
+        alert('Registaration is successflly')
+        switchFormAuto()
+    }
+
+    if (!signIn && confirmPassword.value.trim() !== password.value.trim()) {
+        alert('Password mismatch')
+        return;
+    }
+
+})
+
+//* switch form autho for sign in and Sign Up
+function switchFormAuto(){
     signIn = !signIn
     if (signIn) {
         formTitle.textContent = 'Sign In'
@@ -23,14 +74,12 @@ document.body.addEventListener('click', (e) => {
         authButton.textContent = 'Sign In'
         username.value = ''
         confirmPassword.value = ''
-
+        email.value = ""
+        password.value= ""
         authSwitch.innerHTML = `
       New to DugFlix? <a href="#" id="switchForm">Register now</a>
     `
-
     } else {
-
-
         formTitle.textContent = 'Sign Up'
         username.style.display = 'block'
         confirmPassword.style.display = 'block'
@@ -41,27 +90,4 @@ document.body.addEventListener('click', (e) => {
     `
 
     }
-
-
-})
-
-///?? form submit
-authForm.addEventListener('submit', function(e){
-
-    e.preventDefault()
-    const user = {
-        username : signIn ? undefined : username.value,
-        email : email.value,
-        password : password.value,
-        confirmPassword: signIn ? undefined : confirmPassword.value
-     
-    }
-    
-    if(!signIn && confirmPassword.value.trim() !== password.value.trim()){
-        alert('Password mismatch')
-        return;
-    }
-    localStorage.setItem('users', JSON.stringify(user))
-  
-  
-})
+}
